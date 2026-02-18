a26706fbdf097dd8204cc7337ad644fdbe2fde2f
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-18 21:19:32 +0530

    added api response wrapper in controller

--- src/main/java/com/pramodvaddiraju/eventlite/controller/EventController.java
+++ src/main/java/com/pramodvaddiraju/eventlite/controller/EventController.java
@@ -2,6 +2,8 @@ package com.pramodvaddiraju.eventlite.controller;
 
 import com.pramodvaddiraju.eventlite.dto.EventRequest;
 import com.pramodvaddiraju.eventlite.dto.EventResponse;
+import com.pramodvaddiraju.eventlite.entity.Event;
+import com.pramodvaddiraju.eventlite.response.ApiResponse;
 import com.pramodvaddiraju.eventlite.service.EventService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
@@ -22,32 +24,71 @@ public class EventController {
         this.eventService = eventService;
     }
 
-
     @PostMapping
-    ResponseEntity<EventResponse> createEvent(@Valid @RequestBody EventRequest eventRequest) {
-        return ResponseEntity.status(201).body(eventService.createEvent(eventRequest));
+    ResponseEntity<ApiResponse<EventResponse>> createEvent(@Valid @RequestBody EventRequest eventRequest){
+        EventResponse response = eventService.createEvent(eventRequest);
+        ApiResponse<EventResponse> apiResponse = new ApiResponse<>(true, "Event Created", response);
+        return ResponseEntity.ok(apiResponse);
     }
 
     @GetMapping
-    ResponseEntity<Page<EventResponse>> getAllEvents(Pageable pageable){
-        return ResponseEntity.ok().body(eventService.getAllEvents(pageable));
+    ResponseEntity<ApiResponse<Page<EventResponse>>> getAllEvents(Pageable pageable){
+        Page<EventResponse> events = eventService.getAllEvents(pageable);
+        ApiResponse<Page<EventResponse>> apiResponse = new ApiResponse<>(true, "Fetched all", events);
+        return ResponseEntity.ok(apiResponse);
     }
 
     @GetMapping("/{id}")
-    ResponseEntity<EventResponse> getEventById(@PathVariable Long id){
-        return ResponseEntity.ok().body(eventService.getEventById(id));
+    ResponseEntity<ApiResponse<EventResponse>> getById(@PathVariable Long id){
+        EventResponse event = eventService.getEventById(id);
+        ApiResponse<EventResponse> apiResponse = new ApiResponse<>(true, "Event fetched by id", event);
+        return ResponseEntity.ok(apiResponse);
     }
 
     @PutMapping("/{id}")
-    ResponseEntity<EventResponse> updateEvent(@Valid @PathVariable Long id, @RequestBody EventRequest eventRequest){
-        return ResponseEntity.ok().body(eventService.updateEvent(id, eventRequest));
+    ResponseEntity<ApiResponse<EventResponse>> updateEvent(@Valid @PathVariable Long id, @RequestBody EventRequest eventRequest){
+        EventResponse event = eventService.updateEvent(id, eventRequest);
+        ApiResponse<EventResponse> apiResponse = new ApiResponse<>(true, "Event Updated Successfully", event);
+        return ResponseEntity.ok(apiResponse);
+    }
+    @DeleteMapping("/{id}")
+    ResponseEntity<ApiResponse<Void>> deleteEvent(@PathVariable Long id){
+        eventService.deleteEvent(id);
+        ApiResponse<Void> apiResponse = new ApiResponse<>(true, "Event Deleted Successfully", null);
+        return ResponseEntity.ok(apiResponse);
     }
 
-    @DeleteMapping("/{id}")
-    ResponseEntity<Void> deleteEvent(@PathVariable Long id){
-        eventService.deleteEvent(id);
-        return ResponseEntity.noContent().build();
-    }
+
+
+
+
+
+//
+//    @PostMapping
+//    ResponseEntity<EventResponse> createEvent(@Valid @RequestBody EventRequest eventRequest) {
+//        return ResponseEntity.status(201).body(eventService.createEvent(eventRequest));
+//    }
+//
+//    @GetMapping
+//    ResponseEntity<Page<EventResponse>> getAllEvents(Pageable pageable){
+//        return ResponseEntity.ok().body(eventService.getAllEvents(pageable));
+//    }
+//
+//    @GetMapping("/{id}")
+//    ResponseEntity<EventResponse> getEventById(@PathVariable Long id){
+//        return ResponseEntity.ok().body(eventService.getEventById(id));
+//    }
+//
+//    @PutMapping("/{id}")
+//    ResponseEntity<EventResponse> updateEvent(@Valid @PathVariable Long id, @RequestBody EventRequest eventRequest){
+//        return ResponseEntity.ok().body(eventService.updateEvent(id, eventRequest));
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    ResponseEntity<Void> deleteEvent(@PathVariable Long id){
+//        eventService.deleteEvent(id);
+//        return ResponseEntity.noContent().build();
+//    }
 
 
 
---
+++ src/main/java/com/pramodvaddiraju/eventlite/response/ApiResponse.java
@@ -0,0 +1,43 @@
+package com.pramodvaddiraju.eventlite.response;
+
+import com.pramodvaddiraju.eventlite.dto.EventResponse;
+import org.springframework.context.annotation.Configuration;
+
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public ApiResponse() {
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}

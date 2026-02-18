e2ca756e5f206892af429988c0fada066c7afcd6
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-18 16:59:07 +0530

    initial commit

---
+++ src/main/java/com/pramodvaddiraju/eventlite/EventliteApplication.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.eventlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramodvaddiraju/eventlite/EventliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.eventlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
